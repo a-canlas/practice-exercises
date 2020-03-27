7e789c4b2c395cf9a908bb89c2de5718e8493f3f
matchashiba <recr38@gmail.com>
2020-03-27 19:11:21 +0000

    Completed Edabit Minutes to Seconds challenge

---
+++ edaMinutesToSeconds.js
@@ -0,0 +1,10 @@
+function minutesToSeconds(time) {
+  let timeArr = time.split(':');
+  let minutes = parseInt(timeArr[0]);
+  let seconds = parseInt(timeArr[1]);
+  minutes *= 60;
+  return seconds >= 60 ? false : minutes + seconds;
+
+}
+
+// Convert MM:SS into seconds. If SS is 60 or greater, return false. Otherwise, return the total seconds
